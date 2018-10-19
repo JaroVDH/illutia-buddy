c0db70475a6e811851fb031f42a623b609f2fa04
Jaro Vanderheijden <jaro.vanderheijden@persgroep.be>
2018-10-19 17:20:38 +0200

    Show quant on drop

--- src/command-processor.js
+++ src/command-processor.js
@@ -24,7 +24,7 @@ function processCommand(command, remoteSocket, localSocket) {
 		case ItemDrop: {
 			for (let item in notifyOnItem) {
 				if (notifyOnItem.hasOwnProperty(item) && command.name.indexOf(item) !== -1 && command.quantity >= notifyOnItem[item]) {
-					const message = new Message(Message.types.blue, `${command.name} nearby!`);
+					const message = new Message(Message.types.blue, `${command.quantity} x ${command.name} dropped nearby!`);
 
 					writeToLocal(message.toCommandString());
 				}
