64322e57348e0c4fc4e6aeef37db7e5dbc5b691c
Jaro Vanderheijden <jaro.vanderheijden@persgroep.be>
2018-10-21 20:01:51 +0200

    Notify on mob spawn added to actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,11 @@
 const NotifyOnItemDrop = require('./NotifyOnItemDrop');
+const NotifyOnMobSpawn = require('./NotifyOnMobSpawn');
 const QuestCreditTracker = require('./QuestCreditTracker');
 const UpdateActivePlayer = require('./UpdateActivePlayer');
 
 module.exports = {
 	NotifyOnItemDrop,
+	NotifyOnMobSpawn,
 	QuestCreditTracker,
 	UpdateActivePlayer,
 };
